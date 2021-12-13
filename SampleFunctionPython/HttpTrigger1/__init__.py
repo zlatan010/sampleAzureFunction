efe98e8865afa49c08cfa408ce1d4d5933cb725d
zlatan010 <zlatan0100@hotmail.com>
2021-12-13 16:53:22 +0100

    first version

---
+++ SampleFunctionPython/HttpTrigger1/__init__.py
@@ -0,0 +1,25 @@
+import logging
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(
+            f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "Function executed successfully.",
+             status_code=200
+        )
